7ce68c32ee3d7b904f035be9754d6de5ccae0452
Inam Ilyas <inaamilyas656@gmail.com>
2024-07-27 03:34:07 +0500

    login and signup screens

--- app/src/main/java/com/example/eventplanner/SignupActivity.java
+++ app/src/main/java/com/example/eventplanner/SignupActivity.java
@@ -1,7 +1,6 @@
 package com.example.eventplanner;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.eventplanner.databinding.ActivityLoginBinding;
+import com.example.eventplanner.databinding.ActivitySignupBinding;
 
-public class LoginActivity extends AppCompatActivity {
-ActivityLoginBinding binding;
+public class SignupActivity extends AppCompatActivity {
+    private ActivitySignupBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 }
