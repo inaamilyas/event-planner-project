a849fff9e16eaa805647bb964e204aeeb5f419b3
inamilyas <inam@galixo.ai>
2024-12-18 20:40:37 +0500

    final code

--- app/src/main/java/com/example/eventplanner/config/AppConfig.java
+++ app/src/main/java/com/example/eventplanner/config/AppConfig.java
@@ -1,8 +1,8 @@
 package com.example.eventplanner.config;
 
 public class AppConfig {
-        public static final String SERVER_URL = "https://0623-2400-adcc-110c-3400-cd75-7777-e223-debe.ngrok-free.app";
-//    public static final String SERVER_URL = "http://13.49.36.181:3000";
+//        public static final String SERVER_URL = "https://0623-2400-adcc-110c-3400-cd75-7777-e223-debe.ngrok-free.app";
+    public static final String SERVER_URL = "http://23.150.248.240:3000";
     public static final String SHARED_PREF_NAME = "EventPlannerPrefs";
     public static final String WEATHER_API_KEY = "9f28422c6a2644958a162539241108";
 }
